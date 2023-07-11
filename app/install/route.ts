1e3d001e3b61103e4af143101605027852c9f4ea
Nuno Góis <github@nunogois.com>
2023-07-11 15:41:05 +0100

    fix: correct status code

--- app/install/route.ts
+++ app/install/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   return NextResponse.redirect(
-    `https://slack.com/oauth/v2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&scope=${process.env.NEXT_PUBLIC_SCOPE}`
+    `https://slack.com/oauth/v2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&scope=${process.env.NEXT_PUBLIC_SCOPE}`,
+    { status: 302 }
   )
 }
